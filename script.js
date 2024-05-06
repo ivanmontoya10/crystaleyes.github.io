14649f6ccd94977cded3454bb79e6b0e71e4b13d
ivanmontoya10 <montoya.ivan1am@gmail.com>
2024-05-06 09:00:05 -0600

    buscar

--- script.js
+++ script.js
@@ -20,13 +20,13 @@
 //         .then(data => {
 //             const mostrarProductos = () => {
 //                 const productosDiv = document.getElementById('productos');
-               
+
 //                 productosDiv.innerHTML = '';
 
 //                 data.forEach(producto => {
 //                     const productoDiv = document.createElement('div');
 //                     productoDiv.classList.add('producto-card');
-                    
+
 //                     if (producto.id === 1) {
 //                         productoRedirigir = producto;
 //                         productoDiv.classList.add('producto-card');
@@ -40,7 +40,7 @@
 //                             </div>
 //                         `;
 //                     } else {
-                        
+
 //                         productoDiv.innerHTML = `
 //                             <div>
 //                                 <img src="${producto.img1}" alt="${producto.nombre}" title="${producto.nombre}">
@@ -73,9 +73,26 @@
 //     });
 // });
 
-const nav = document.querySelector("#nav");
-const abrir = document.querySelector("#abrir");
-const cerrar = document.querySelector("#cerrar");
+// const nav = document.querySelector("#nav");
+// const abrir = document.querySelector("#abrir");
+// const cerrar = document.querySelector("#cerrar");
+
+// abrir.addEventListener("click", () => {
+//     nav.classList.add("visible");
+// });
+
+// cerrar.addEventListener("click", () => {
+//     nav.classList.remove("visible");
+// });
+
+// const headerURL = 'header.html';
+
+// fetch(headerURL)
+//     .then(response => response.text())
+//     .then(html => {
+//         document.getElementById('header-container').innerHTML = html;
+//     })
+//     .catch(error => console.error('Error al cargar el encabezado:', error));
 
 abrir.addEventListener("click", () => {
     nav.classList.add("visible");
@@ -85,15 +102,118 @@ cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 });
 
-// Ruta del archivo del encabezado
-const headerURL = 'header.html';
+let productosCargados = false; // Variable para verificar si los productos ya se cargaron
+document.addEventListener("DOMContentLoaded", () => {
+    // Verificar si los productos ya se cargaron antes
+    if (!productosCargados) {
+        // Obtener el parámetro de búsqueda de la URL
+        const params = new URLSearchParams(window.location.search);
+        const query = params.get('query');
 
-// Cargar el contenido del encabezado
-fetch(headerURL)
-    .then(response => response.text())
-    .then(html => {
-        // Insertar el contenido del encabezado en el contenedor
-        document.getElementById('header-container').innerHTML = html;
+        // Si hay una consulta de búsqueda, resaltar o mostrar primero el producto que coincide
+        if (query) {
+            fetch('/crystaleyes/jsons/productos.json')
+                .then(response => response.json())
+                .then(data => {
+                    const productosCoincidentes = data.filter(producto => {
+                        const nombreProducto = producto.nombre.toLowerCase().trim();
+                        const queryLowerCase = query.toLowerCase().trim();
+                        return nombreProducto.includes(queryLowerCase);
+                    });
+                    // Si se encontraron productos coincidentes
+                    if (productosCoincidentes.length > 0) {
+                        // Renderizamos los productos coincidentes
+                        renderizarProductos(productosCoincidentes);
+                    } else {
+                        // Si no se encontraron productos, mostrar un mensaje de alerta
+                        alert("No se encontraron productos que coincidan con la búsqueda.");
+                    }
+                })
+                .catch(error => console.error('Error al procesar los datos JSON:', error));
+        } else {
+            // Si no hay consulta de búsqueda, simplemente cargar todos los productos
+            cargarProductos();
+        }
+        productosCargados = true; // Marcar que los productos han sido cargados
+    }
+    // Event listener para el cambio en el selector de orden
+    const selectorOrden = document.getElementById('ordenar');
+    selectorOrden.addEventListener('change', () => {
+        renderizarProductos(productos); // Llama a la función renderizarProductos con los productos actuales
+    });
+});
+
+// Función para cargar todos los productos
+const cargarProductos = () => {
+    fetch('http://localhost/crystaleyes/jsons/productos.json', {
+        method: 'GET',
+        credentials: 'include'
     })
-    .catch(error => console.error('Error al cargar el encabezado:', error));
+        .then(response => response.json())
+        .then(data => {
+            renderizarProductos(data);
+        })
+        .catch(error => console.error('Error al cargar el archivo JSON:', error));
+};
 
+// Función para renderizar y ordenar los productos
+const renderizarProductos = (productos) => {
+    // Obtener el criterio de orden seleccionado
+    const criterio = document.getElementById('ordenar').value;
+
+    // Obtener el parámetro de búsqueda de la URL
+    const params = new URLSearchParams(window.location.search);
+    const query = params.get('query');
+
+    // Filtrar los productos según la búsqueda si existe
+    let productosFiltrados = productos;
+    if (query) {
+        productosFiltrados = productos.filter(producto => {
+            const nombreProducto = producto.nombre.toLowerCase().trim();
+            const queryLowerCase = query.toLowerCase().trim();
+            return nombreProducto.includes(queryLowerCase);
+        });
+    }
+
+    // Aplicar el filtro según el criterio seleccionado
+    switch (criterio) {
+        case 'precio_asc':
+            productosFiltrados.sort((a, b) => a.precio - b.precio);
+            break;
+        case 'precio_desc':
+            productosFiltrados.sort((a, b) => b.precio - a.precio);
+            break;
+        case 'nombre_asc':
+            productosFiltrados.sort((a, b) => a.nombre.localeCompare(b.nombre));
+            break;
+        case 'nombre_desc':
+            productosFiltrados.sort((a, b) => b.nombre.localeCompare(a.nombre));
+            break;
+        default:
+            break;
+    }
+
+    const productosDiv = document.getElementById('productos');
+    productosDiv.innerHTML = '';
+
+    productosFiltrados.forEach(producto => {
+        const productoDiv = crearCardProducto(producto);
+        productosDiv.appendChild(productoDiv);
+    });
+};
+
+// Función para crear una tarjeta de producto
+const crearCardProducto = (producto) => {
+    const productoDiv = document.createElement('div');
+    productoDiv.classList.add('producto-card');
+    productoDiv.innerHTML = `
+        <div>
+            <img src="${producto.img1}">
+            <h2>${producto.nombre}</h2>
+            <p class="precio">$${producto.precio}</p>
+            <p>${producto.descripcion_larga}</p>
+            <center><button onclick="verDetalle(${producto.id})">Ver producto</button></center>
+        </div>
+    `;
+    return productoDiv;
+};
